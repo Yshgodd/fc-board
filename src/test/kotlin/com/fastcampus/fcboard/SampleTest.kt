cc525aa9f607a0fd169f87327ad08a2e7ec4f00b
Yshgodd <kyun20087@naver.com>
2025-05-16 14:01:37 +0900

    add first commit

---
+++ src/test/kotlin/com/fastcampus/fcboard/SampleTest.kt
@@ -0,0 +1,10 @@
+package com.fastcampus.fcboard
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+
+class SampleTest : FunSpec({
+    test("test") {
+        1 shouldBe 1
+    }
+})
